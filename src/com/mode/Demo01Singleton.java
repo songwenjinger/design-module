bbf516d5a03d64c99d7df75d30bb40d977bbb4ee
宋文静 <songwenjing@songweningdeMBP.lan>
2022-08-21 15:20:03 +0800

    Initial commit

---
+++ src/com/mode/Demo01Singleton.java
@@ -0,0 +1,27 @@
+package com.mode;
+
+/**
+ * 单例模式
+ *
+ * 使用场景：需要共享数据 I/o ，数据库链接等 springIOC
+ */
+public class Demo01Singleton {
+
+    //volatile为了防止指令重排序
+    private static volatile Demo01Singleton instance;
+
+    private Demo01Singleton() {
+    }
+
+    public static Demo01Singleton createSingleton() {
+        if (instance == null) {
+            synchronized (Demo01Singleton.class) {
+                if (instance == null) {
+                    instance = new Demo01Singleton();
+                }
+                return instance;
+            }
+        }
+        return instance;
+    }
+}
---
+++ src/com/mode/Demo02EasyFactory.java
@@ -0,0 +1,63 @@
+package com.mode;
+
+import java.util.Objects;
+
+/**
+ * 简单工厂模式
+ */
+public class Demo02EasyFactory {
+    public abstract class Operation {
+        private Integer a;
+        private Integer b;
+
+        public Integer getA() {
+            return a;
+        }
+
+        public void setA(Integer a) {
+            this.a = a;
+        }
+
+        public Integer getB() {
+            return b;
+        }
+
+        public void setB(Integer b) {
+            this.b = b;
+        }
+
+        public abstract Integer getOperate();
+    }
+
+    public class AddOperation extends Operation {
+        @Override
+        public Integer getOperate() {
+            return getA() + getB();
+        }
+    }
+
+    public class SubOperation extends Operation {
+        @Override
+        public Integer getOperate() {
+            return getA() - getB();
+        }
+    }
+
+    public Operation createOperation(String s) {
+        if (Objects.equals(s, "add")) {
+            return new AddOperation();
+        } else {
+            return new SubOperation();
+        }
+    }
+
+    public static void main(String[] args) {
+        Demo02EasyFactory factory=new Demo02EasyFactory();
+
+        Operation add = factory.createOperation("add");
+        add.setA(1);
+        add.setB(3);
+        System.out.println(add.getOperate());
+
+    }
+}
